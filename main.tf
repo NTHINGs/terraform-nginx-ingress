f6e8df778ec2573e10e446685c5b4c1426ba0d8e
Mauricio Martinez <n_othing@hotmail.com>
2020-03-01 18:31:10 -0600

    Migrating controller to this repo

---
+++ main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    helm = "~> 1.0"
+  }
+}
+
+data "terraform_remote_state" "cluster" {
+  backend = "remote"
+
+  config = {
+    hostname     = "app.terraform.io"
+    organization = "tecnoly"
+
+    workspaces {
+      name = "gke-cluster"
+    }
+  }
+}
+
+data "google_client_config" "current" {}
+
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = "https://${data.terraform_remote_state.cluster.outputs.cluster_endpoint}"
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster.outputs.cluster_ca_certificate)
+  token                  = data.google_client_config.current.access_token
+}
+
+resource "kubernetes_namespace" "nginx_ingress" {
+  metadata {
+    name = "nginx-ingress"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    load_config_file       = false
+    host                   = "https://${data.terraform_remote_state.cluster.outputs.cluster_endpoint}"
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster.outputs.cluster_ca_certificate)
+    token                  = data.google_client_config.current.access_token
+  }
+}
+
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
+resource "helm_release" "nginx_ingress" {
+  name          = "nginx-ingress"
+  repository    = data.helm_repository.stable.metadata[0].name
+  chart         = "nginx-ingress"
+  version       = "1.33.0"
+  namespace     = kubernetes_namespace.nginx_ingress.id
+
+  values = [
+    file("${path.module}/values.yaml"),
+  ]
+}
